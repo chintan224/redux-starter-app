bea0906a977e176c56badcd096650eb2a5ceecfa
chintan224 <161326876+chintan224@users.noreply.github.com>
2024-03-02 12:14:40 +0530

    added itemreducer

---
+++ src/state/action-creators/itemReducer.js
@@ -0,0 +1,19 @@
+import { ADD_ITEM,REMOVE_ITEM } from "./actionTypes";
+const initialState = {
+    items:[]
+}
+
+const itemReducer = ( state = initialState, action)=> {
+    switch(action.type){
+        case ADD_ITEM :
+            return {...state,
+                items:[...state.items,action.payload]};
+        case REMOVE_ITEM : 
+        return {...state,
+            items:state.items.filter(item=>item.id!==action.payload)};
+        default :
+        return state;
+    }
+}
+
+export default itemReducer;
